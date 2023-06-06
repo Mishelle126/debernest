6d0a19e8981db253c8842d6a39cb2fe5bebdfe6e
Mishelle126 <97122505+Mishelle126@users.noreply.github.com>
2023-06-06 16:36:43 -0500

    tarea_entidades_nestjs

--- src/models/autor.models.ts
+++ src/models/autor.models.ts
@@ -1,45 +1,47 @@
 import { Column, CreateDateColumn, DeleteDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn, OneToMany } from "typeorm";
+import { LibroEntity } from "./libro.model";
 
 
 @Entity('autores', { schema: 'ventas' })
 export class AutorEntity {
-    @PrimaryGeneratedColumn('uuid')
-    id: string;
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
 
-    @CreateDateColumn({
-        name: 'create_at',
-        type: 'timestamp',
-        default: () => 'CURRENT_TIMESTAMP',
-    })
-    createAt: Date;
+  @CreateDateColumn({
+    name: 'create_at',
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  createAt: Date;
 
-    @UpdateDateColumn({
-        name: 'update_at',
-        type: 'timestamp',
-        default: () => 'CURRENT_TIMESTAMP',
-    })
-    updateAt: Date;
+  @UpdateDateColumn({
+    name: 'update_at',
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  updateAt: Date;
 
-    @DeleteDateColumn({
-        name: 'delete_at',
-        type: 'timestamp',
-        nullable: true,
-    })
-    deleteAt: Date;
+  @DeleteDateColumn({
+    name: 'delete_at',
+    type: 'timestamp',
+    nullable: true,
+  })
+  deleteAt: Date;
 
-    @Column('varchar', {
-        name: 'nombre',
-        nullable: false,
-        comment: 'nombre del autor',
-    })
-    nombre: string;
+  @Column('varchar', {
+    name: 'nombre',
+    nullable: false,
+    comment: 'nombre del autor',
+  })
+  nombre: string;
 
-    @Column('varchar', {
-        name: 'descripcion',
-        nullable: true,
-        comment: 'descripción del autor',
-    })
-    descripcion: string;
+  @Column('varchar', {
+    name: 'descripcion',
+    nullable: true,
+    comment: 'descripción del autor',
+  })
+  descripcion: string;
 
-   
+  @OneToMany(() => LibroEntity, libro => libro.autor)
+  libros: LibroEntity[];
 }
--- src/models/libro.model.ts
+++ src/models/libro.model.ts
@@ -1,47 +1,47 @@
 import { Column, CreateDateColumn, DeleteDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn, ManyToOne, JoinColumn } from "typeorm";
 import { AutorEntity } from "./autor.models";
 
-
 @Entity('libros', { schema: 'ventas' })
 export class LibroEntity {
-    @PrimaryGeneratedColumn('uuid')
-    id: string;
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
 
-    @CreateDateColumn({
-        name: 'create_at',
-        type: 'timestamp',
-        default: () => 'CURRENT_TIMESTAMP',
-    })
-    createAt: Date;
+  @CreateDateColumn({
+    name: 'create_at',
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  createAt: Date;
 
-    @UpdateDateColumn({
-        name: 'update_at',
-        type: 'timestamp',
-        default: () => 'CURRENT_TIMESTAMP',
-    })
-    updateAt: Date;
+  @UpdateDateColumn({
+    name: 'update_at',
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  updateAt: Date;
 
-    @DeleteDateColumn({
-        name: 'delete_at',
-        type: 'timestamp',
-        nullable: true,
-    })
-    deleteAt: Date;
+  @DeleteDateColumn({
+    name: 'delete_at',
+    type: 'timestamp',
+    nullable: true,
+  })
+  deleteAt: Date;
 
-    @Column('varchar', {
-        name: 'titulo',
-        nullable: false,
-        comment: 'título del libro',
-    })
-    titulo: string;
+  @Column('varchar', {
+    name: 'titulo',
+    nullable: false,
+    comment: 'título del libro',
+  })
+  titulo: string;
 
-    @Column('varchar', {
-        name: 'descripcion',
-        nullable: true,
-        comment: 'descripción del libro',
-    })
-    descripcion: string;
+  @Column('varchar', {
+    name: 'descripcion',
+    nullable: true,
+    comment: 'descripción del libro',
+  })
+  descripcion: string;
 
-    autor:AutorEntity;
-
+  @ManyToOne(() => AutorEntity, autor => autor.libros)
+  @JoinColumn({ name: 'autor_id' }) // Nombre de la columna que almacena el id del autor en la tabla libros
+  autor: AutorEntity;
 }
